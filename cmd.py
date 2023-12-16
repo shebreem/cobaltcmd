31520cec7c33b4eb1a330fe053bef7437bd69a4b
leon <145664881+shebreem@users.noreply.github.com>
2023-12-16 12:57:56 +0000

    Update cmd.py

--- cmd.py
+++ cmd.py
@@ -1,6 +1,23 @@
-import os
+import socket
+import time
+
+def get_ip_address(website):
+    try:
+        hostname = socket.gethostbyname(website)
+        return hostname
+    except Exception as e:
+        print(f"Error occurred: {e}")
+        return None
+
+def command():
+    com = input("You: ")
+    if "webip" in com:
+        website = input("Enter the website: ")
+        ip_address = get_ip_address(website)
+        if ip_address:
+            print(f"IP Address for {website}: {ip_address}")
 
 while True:
-    print("welcome to cobalt terminal")
-    command = str(input(""))
-    os.system(command)
+    print("Welcome to the cobalt terminal")
+    command()
+    time.sleep(1)
